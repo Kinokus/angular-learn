0329257b15b20ee2ffc2c162c030b4739237e4ae
Aleks <aleks.kinokus@gmail.com>
2022-07-30 13:32:18 +0300

    lot of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, UserComponent, ProductComponent, CategoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-user',
+  template:`
+    <div>User component</div>
+  `
+})
+export class UserComponent {}
